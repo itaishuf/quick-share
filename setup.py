dbfdfe14d9e01ad73a4937b156c9f3bddf96ac2c
Itai <itaishuf@gmail.com>
2025-08-03 11:26:36 +0300

    fixed bug in setup script

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ if os.path.exists('requirements.txt'):
 
 script_path = os.path.abspath('service.pyw')
 python_executable = sys.executable.replace('python', 'pythonw')
-task_name = 'RunPythonServiceAtStartup'
+task_name = 'RunQuickShareAtStartup'
 vbs_filename = os.path.join(os.environ['APPDATA'],
                             'Microsoft\\Windows\\Start Menu\\Programs\\Startup',
                             f'{task_name}.vbs')
 
 with open(vbs_filename, 'w') as vbs_file:
         vbs_file.write(f'CreateObject("Wscript.Shell").Run """{python_executable}"" ""{script_path}""", 0, True')
-# with open(bat_filename, 'w') as bat_file:
-#         bat_file.write(f'@echo off\n"{python_executable}" "{script_path}"\n')
+
 print(f'Windows task created at {vbs_filename}')
